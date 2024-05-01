d875a278855267a4e9579104664902163dd21c30
oluwabajio <ismailosunlana@yahoo.com>
2024-05-01 16:21:04 +0100

    Initial Commit

---
+++ bold Text Generator/bold_Text_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  bold_Text_GeneratorApp.swift
+//  bold Text Generator
+//
+//  Created by oluwabajio on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct bold_Text_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
